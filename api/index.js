5abfe6dfb27ace9d1f094b0e8f67e7d29bc3ad5e
NIkhil Vanjari <nickvanjari22@gmail.com>
2023-10-19 22:15:27 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const userRoute=require("./routes/user");
+const productRoute=require("./routes/product");
+const authRoute=require("./routes/auth");
+const orderRoute=require("./routes/order");
+const cartRoute=require("./routes/cart");
+const stripeRoute=require("./routes/stripe");
+const cors=require("cors");
+
+const app=express();
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB Connection Successfull!"))
+.catch((err)=>{
+    console.log(err);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/user",userRoute);
+app.use("/api/product",productRoute);
+app.use("/api/cart",cartRoute);
+app.use("/api/order",orderRoute);
+app.use("/api/checkout",stripeRoute);
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("backend server is running !");
+});
